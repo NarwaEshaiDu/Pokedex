9e57599b6474c36bb8ff4f4b9b4e9fbd8a3965ea
unknown <shayan.khademi.1994@gmail.com>
2020-11-01 20:25:06 +0100

    Oefening 3

---
+++ Pokedex/Controllers/PokedexController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pokedex.Models;
+
+namespace Pokedex.Controllers
+{
+    public class PokedexController : Controller
+    {
+        private List<Pokemon> getPokemon()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+            var lines = System.IO.File.ReadAllText(@"pokedex.json");
+            return JsonSerializer.Deserialize<List<Pokemon>>(lines, options);
+        }
+        private readonly ILogger <PokedexController> logger;
+        public PokedexController(ILogger<PokedexController> logger )
+        {
+            this.logger = logger;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.TimeStampGenerated = DateTime.Now.ToString();
+            logger.LogInformation("Someone visited the pokedex");
+            return View(); 
+        }
+        public IActionResult Details(int id)
+        {
+            List<Pokemon> pokemon = getPokemon();
+            foreach (var p in pokemon)
+            {
+                if (p.Id==id)
+                {
+                    return View(p);
+                }
+            }
+            return NotFound();
+            
+        }
+    }
+}
